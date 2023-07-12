b2a05da53bc3d457b6025b6f7440347949d4d1d1
NahlaBZ <benzidnahla@gmail.com>
2023-07-13 00:39:05 +0200

    Ajout d'un middleware d'authentification

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jsonWebToken = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = (req, res, next) => {
+    try {
+
+        const token = req.headers.authorization.split(' ')[1];
+
+        const decodedToken = jsonWebToken.verify(token, process.env.CLE_SECRETE);
+
+        const userId = decodedToken.userId;
+        req.auth = {
+            userId: userId
+        };
+        next();
+    } catch (error) {
+        res.status(401).json({ error });
+    }
+};
