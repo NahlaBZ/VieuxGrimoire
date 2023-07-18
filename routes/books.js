1a22a8f088a4a83f406a58fa96075b6872abdd91
NahlaBZ <benzidnahla@gmail.com>
2023-07-18 11:06:34 +0200

    Apllication du middleware auth et controllers book à la route books

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const Ctrlbooks = require('../controllers/Book');
+const auth = require('../middleware/auth');
 
-
-router.post('/');
+router.post('/', auth, Ctrlbooks.createBook);
 router.post('/:id/rating');
 router.put('/:id');
 router.delete('/:id');
