26430fcf196dd42df1e8374f03a081145d3ce95d
NahlaBZ <benzidnahla@gmail.com>
2023-07-24 00:14:01 +0200

    Ajout de la fonction de modification

--- controllers/Book.js
+++ controllers/Book.js
@@ -53,3 +53,88 @@ exports.deleteBook = (req, res, next) => {
         })
         .catch(error => { res.status(500).json({ error }) });
 };
+
+exports.modifyBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+        .then((book) => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({ message: 'Non-autorisé' });
+            }
+            else if (req.file) {
+                const bookObject = {
+                    ...JSON.parse(req.body.book),
+                    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.path}`
+                };
+
+                const fileToDelete = book.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${fileToDelete}`, () => {
+                    Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Livre modifié !' }))
+                        .catch(error => { res.status(400).json({ error }) })
+                });
+
+            } else {
+                const bookObject = {
+                    ...req.body
+                };
+                if (book.userId != req.auth.userId) {
+                    res.status(401).json({ message: 'Non-autorisé' });
+                } else {
+                    Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Livre modifié !' }))
+                        .catch(error => { res.status(400).json({ error }) })
+                }
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error })
+        });
+};
+
+
+exports.addRating = (req, res, next) => {
+    const ratingObject = req.body;
+    ratingObject.grade = ratingObject.rating;
+    delete ratingObject.rating;
+
+    Book.findOne({ _id: req.params.id })
+        .then(book => {
+            if (!book) {
+                res.status(404).json({ message: 'Livre inconnu' });
+            } else {
+                // On vérifie si l'utilisateur a déja noté le livre
+                const userRating = book.ratings.includes(rating => rating.userId == req.body.userId);
+                if (userRating) {
+                    // s'il l'a déja noté, message d'erreur
+                    res.status(404).json({ message: 'Vous avez déja noté ce livre' });
+                } else {
+                    // sinon on ajoute la nouvelle note au tableau
+                    Book.updateOne({ _id: req.params.id }, { $push: { ratings: ratingObject } })
+                        .then(() => {
+                            Book.findOne({ _id: req.params.id })
+                                .then(book => {
+                                    // on recalcule la moyenne
+                                    const somme = book.ratings.reduce((acc, rating) => acc + rating.grade, 0);
+                                    book.averageRating = Math.round(somme / book.ratings.length);
+
+                                    Book.updateOne({ _id: req.params.id }, { averageRating: book.averageRating })
+                                        .then(() => {
+                                            Book.findOne({ _id: req.params.id })
+                                                .then(book => {
+                                                    res.status(200).json(book)
+                                                })
+                                                .catch(error => res.status(404).json({ error }));
+                                        })
+                                        .catch(error => res.status(401).json({ error }));
+                                })
+                                .catch(error => res.status(404).json({ error }));
+                        })
+                        .catch(error => res.status(400).json({ error }));
+                }
+            }
+        })
+        .catch(error => { res.status(500).json({ error }) });
+}
+
+
+
